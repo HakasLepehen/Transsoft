926541e5c877e410446f1141e62af95b83a0b513
Pavel Petrov <doctorrosen2016@gmail.com>
2021-03-26 01:46:12 +0600

    starting project

---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MinimizeCssPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const webpack = require('webpack');
+
+console.log(process.env.NODE_ENV);
+console.log(__dirname);
+const isDev = process.env.NODE_ENV === "development";
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all"
+        }
+    };
+
+    if (!isDev) {
+        config.minimizer = [new MinimizeCssPlugin(), new TerserWebpackPlugin(),];
+    }
+
+    return config;
+};
+
+const cssLoaders = (extra) => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                publicPath: ""
+            }
+        }, {
+            loader: "css-loader"
+        },
+    ]
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders;
+}
+
+console.log('IsDev:', isDev);
+
+module.exports = {
+    context: path.resolve(__dirname),
+    entry: path.join(__dirname, "src", "index.js"),
+    output:
+        {
+            filename: "[name].[contenthash].js",
+            path: path.resolve(__dirname, "dist")
+        },
+
+    optimization: optimization(),
+
+    devServer: {
+        hot: false,
+        liveReload: true,
+        watchContentBase: true,
+        port: 4200
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin(
+            {
+                title: 'Evermix',
+                template: "./index.html",
+                minify: {
+                    collapseWhitespace: !isDev
+                }
+            }
+        ),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(
+            {filename: "[name].[contenthash].css"}
+        ),
+    ],
+    externalsType: 'script',
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            }, {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(ico|gif|png|jpg|jpeg|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'src/media/[name].[ext]',
+                    publicPath: ''
+                }
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'src/fonts/'
+                        }
+                    }
+                ],
+            },
+        ]
+    }
+};
