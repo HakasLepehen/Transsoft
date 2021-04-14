648fec9e8e2b76d0a3b593634331358966a9489d
HakasLepehen <doctorrosen2016@gmail.com>
2021-04-14 17:37:39 +0600

    adding content in cards

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import './img/logo55.webp';
 import './img/bsmts.webp';
 import './img/video.webp';
 import './img/autopilot.webp';
+import './img/tahograph.webp';
+import './img/podogrev.webp';
 import 'bootstrap';
 import '@popperjs/core';
