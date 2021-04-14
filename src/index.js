ef946ea814326ba8b9fa1873e79d3fa03f474368
HakasLepehen <doctorrosen2016@gmail.com>
2021-04-14 10:40:58 +0600

    small changes in index.html

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './styles/styles.scss';
 import './img/logo55.webp';
 import './img/bsmts.webp';
 import './img/video.webp';
+import './img/autopilot.webp';
 import 'bootstrap';
 import '@popperjs/core';
