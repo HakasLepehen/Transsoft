1af210939a7e1f683772fe6653e08d2f4e110fa7
HakasLepehen <doctorrosen2016@gmail.com>
2021-04-15 17:52:44 +0600

    adding content to cards

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './styles/styles.scss';
+//images
 import './img/logo55.webp';
 import './img/bsmts.webp';
 import './img/video.webp';
 import './img/autopilot.webp';
 import './img/tahograph.webp';
 import './img/podogrev.webp';
+import './img/iridium.webp'
+
+//libraries
 import 'bootstrap';
 import '@popperjs/core';
