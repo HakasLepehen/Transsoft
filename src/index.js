1947b62370a28ee0bda43f534feae5288e6d771a
Pavel Petrov <doctorrosen2016@gmail.com>
2021-04-29 06:47:34 +0600

    working with scripts

--- src/index.js
+++ src/index.js
@@ -13,20 +13,38 @@ import './img/gbo.webp';
 import './img/new1.webp';
 import './img/new2.webp';
 import './img/new3.webp';
-import './img/map-marker.svg'
+import './img/map-marker.svg';
 
 //libraries
 import 'bootstrap';
 import '@popperjs/core';
+import DG from '2gis-maps';
 
-window.onload = function () {
-    let btn = document.querySelectorAll('.card');
-    btn.forEach((el) => {
-        el.addEventListener('click', (e) => {
-            console.log('Нажато:', e.target);
-        })
-    })
+let map = DG.map('map', {
+    'center': [55.022912, 73.263413],
+    'zoom': 17
+})
 
-}
+DG.marker([55.023075, 73.2628]).addTo(map);
+
+// const a = document.querySelectorAll('a[href*="#"]:not([href="#"])');
+// a.forEach((el) => {
+//     el.addEventListener('click', function (e) {
+//
+//         if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+//             || location.hostname === this.hostname) {
+//
+//             let target = $(this.hash);
+//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//             if (target.length) {
+//                 e.preventDefault();
+//                 $('html,body').animate({
+//                     scrollTop: target.offset().top
+//                 }, 500);
+//                 return false;
+//             }
+//         }
+//     })
+// })
 
 
